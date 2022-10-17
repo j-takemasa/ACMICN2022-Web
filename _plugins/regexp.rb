0fdc84c9547a2837f9083cbe0066c12cb7f6a0a3
j-takemasa <48339882+j-takemasa@users.noreply.github.com>
2022-10-17 11:39:32 +0900

    Delete _plugins directory

--- _plugins/regexp.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module RegexFilter
-    def replace_regex(input, reg_str, repl_str)
-      input.to_s.gsub(Regexp.new(reg_str), repl_str.to_s)
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::RegexFilter)
